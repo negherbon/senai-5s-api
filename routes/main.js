c56970801e978972fa2a7287fa2ca076cd0e0514
Guilherme Nass <Guilherme.Nass@dinamio.com.br>
2018-02-23 10:57:42 -0300

    teste

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/main', function(req, res) {
     var notifications = [
-        'teste', 'teste2', 'teste3'
+        'teste', 'teste2', 'teste3', 'teste4'
     ]
     res.json({notifications: notifications})
 });
