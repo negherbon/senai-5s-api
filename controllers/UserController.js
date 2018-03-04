4f8ca3f3c42b1c187010e56bb8a388166a29ccc8
Guilherme Nass <nassguilherme@gmail.com>
2018-03-04 15:07:13 -0300

    ultimas alterações crud de usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,21 +1,15 @@
 var models  = require('../models');
 
 module.exports = class UserController {
-    async save(){
-        var user = {
-            name: 'teste',
-            email: 'nassguilherme@gmail.com',
-            userName: 'nass',
-            profile: 'admin',
-            password: '123456'
-        };
-        
+     save(user){
+        console.log('esse user vem undefined' + user)
+        user.userName = user.email.split("@")[0];
         models.User.create(user)
-            .then(function () {
-                console.log('ok')
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+        .then(function () {
+            console.log('ok')
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
     }
 }
--- server.js
+++ server.js
@@ -9,11 +9,21 @@ const main = require('./routes/main')
 
 process.env.SECRET_KEY = "mybadasskey";
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+    app.use(function(req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
   
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
