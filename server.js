7d9432df83ba93941224d0d70d25d7d1cad7affe
Guilherme Nass <nassguilherme@gmail.com>
2018-03-13 23:31:41 -0300

    ajuste headers para setar authorization

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ process.env.SECRET_KEY = "projeto@senai_5s@";
     app.use(function(req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
         res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
